d93de3544a0c4430d6ee32d9935d2de18f9cb359
Mir Riyanul Islam <mirriyan41@gmail.com>
2017-02-03 16:34:11 +0600

    fonts added

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Mir Riyanul Islam</title>
+	<meta content="width=device-width, initial-scale=1.0" name="viewport" />
+	<meta content="Personal Website" name="description" />
+	<meta content="Mir Riyanul Islam" name="author" />
+	<link href="style.css" rel="stylesheet" type="text/css"/>
+    <!--<link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Architects+Daughter|Open+Sans|Orbitron|Play|Roboto|Sacramento|Sansita" rel="stylesheet">-->     
+    <script type="text/javascript" src="script.js"></script>
+    <script type="text/javascript" src="jquery-1.10.1.min.js"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Abril+Fatface|Architects+Daughter|Open+Sans|Orbitron|Play|Roboto|Sacramento|Sansita');
+    </style> 
+</head>
+<body>
+    <div class="base">
+        <div class="container">
+            <div class="header">
+                <div class="heading">
+                    Mir Riyanul Islam
+                </div>
+                <div class="navpane">
+                    Home | Academic | Research | Contact
+                </div>
+            </div>
+            <div class="content">
+            
+            </div>
+            <div class="footer">
+            
+            </div>
+        </div>
+    </div>
+</body>
+</html>
