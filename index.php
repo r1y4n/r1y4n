62586f77826a4f8849d99f7b77fc6a2c854487f1
r1y4n <mirriyan41@gmail.com>
2017-02-06 01:54:31 +0600

    home page done

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
         @import url('https://fonts.googleapis.com/css?family=Abril+Fatface|Architects+Daughter|Open+Sans|Orbitron|Play|Roboto|Sacramento|Sansita');
     </style>
 </head>
-<body>
+<body onload="init()">
     <div class="base">
-        <div class="container">
+        <div class="container" id="baseid" style="display: none;">
             <div class="header">
                 <div class="heading">
                     Mir Riyanul Islam
@@ -24,20 +24,20 @@
                 <div class="navcontainer">
 					<ul class="navtab" id="tabs">
 						<li><a href="#home" class="tabbuttons selected">Home</a></li>
-						<li><a href="#home" class="tabbuttons">About</a></li>
-						<li><a href="#home" class="tabbuttons">Education</a></li>
-						<li><a href="#home" class="tabbuttons">Experience</a></li>
-						<li><a href="#home" class="tabbuttons">Research</a></li>
-						<li><a href="#home" class="tabbuttons">Contact</a></li>
+						<li><a href="#about" class="tabbuttons">About</a></li>
+						<li><a href="#education" class="tabbuttons">Education</a></li>
+						<li><a href="#work" class="tabbuttons">Work</a></li>
+						<li><a href="#research" class="tabbuttons">Research</a></li>
+						<li><a href="#contact" class="tabbuttons">Contact</a></li>
 					</ul>
                 </div>
             </div>
             <div class="contenttabs">
-				<div class="content" id="home" style="display: block;">
+				<div class="tabcontent" id="home">
 					<div class="table">
 						<div class="row">
 							<div class="col subcontent imagediv">
-								<a class="imga" href="riyan.jpg" target="blank"><img class="profileimage" src="riyan_bw.jpg"></a>
+								<a class="imga" href="riyan.jpg" target="_blank"><img class="profileimage" src="riyan_bw.jpg"></a>
 							</div>
 							<div class="col subcontent introdiv">
 								<h3>Hello I'm Riyan</h3>
@@ -48,9 +48,44 @@
 						</div>
 					</div>
 				</div>
+				<div class="tabcontent hide" id="about">
+					About
+				</div>
+				<div class="tabcontent hide" id="education">
+					Education
+				</div>
+				<div class="tabcontent hide" id="work">
+					Work
+				</div>
+				<div class="tabcontent hide" id="research">
+					Research
+				</div>
+				<div class="tabcontent hide" id="contact">
+					Contact
+				</div>
             </div>
             <div class="footer">
-
+				<div class="sociallinks">
+					<table class="socialtable">
+						<tr>
+							<td><a class="footimga" target="_blank" href="https://www.facebook.com/mirriyan">
+								<img class="socialimg" src="facebook_bw.png" onmouseover="this.src='facebook.png';" onmouseout="this.src='facebook_bw.png';" >
+							</a></td>
+							<td><a class="footimga" target="_blank" href="https://www.linkedin.com/in/mirriyan">
+								<img class="socialimg" src="linkedin_bw.png" onmouseover="this.src='linkedin.png';" onmouseout="this.src='linkedin_bw.png';" >
+							</a></td>
+							<td><a class="footimga" target="_blank" href="https://www.twitter.com/Riyan41">
+								<img class="socialimg" src="twitter_bw.png" onmouseover="this.src='twitter.png';" onmouseout="this.src='twitter_bw.png';" >
+							</a></td>
+							<td><a class="footimga" target="_blank" href="https://plus.google.com/+MirRiyan">
+								<img class="socialimg" src="googleplus_bw.png" onmouseover="this.src='googleplus.png';" onmouseout="this.src='googleplus_bw.png';" >
+							</a></td>
+						</tr>
+					</table>
+				</div>
+				<div class="footnote">
+					2017 &copy; <span class="footname">Mir Riyanul Islam</span>
+				</div>
             </div>
         </div>
     </div>
